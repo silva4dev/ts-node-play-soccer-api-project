999bac11f7c421c21a2e1dfde8e6db39cc255cdb
silva4dev <web4dev.lucas@outlook.com>
2023-11-16 00:51:31 -0300

    feat: add delete event

---
+++ src/domain/usecases/delete-event.ts
@@ -0,0 +1,19 @@
+import type { LoadGroupRepository, DeleteEventRepository, DeleteMatchRepository } from '@/src/domain/repositories'
+
+export class DeleteEvent {
+  constructor (
+    private readonly loadGroupRepository: LoadGroupRepository,
+    private readonly deleteEventRepository: DeleteEventRepository,
+    private readonly deleteMatchRepository: DeleteMatchRepository
+  ) { }
+
+  async perform ({ id, userId }: { id: string, userId: string }): Promise<void> {
+    const group = await this.loadGroupRepository.load({ eventId: id })
+    if (group === undefined) throw new Error()
+    if (group.users.find((user) => user.id === userId) === undefined) throw new Error()
+    if (group.users.find((user) => user.id === userId)?.permission === 'user') throw new Error()
+
+    await this.deleteEventRepository.delete({ id })
+    await this.deleteMatchRepository.delete({ eventId: id })
+  }
+}
